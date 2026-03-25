1e8e16aefa6bb5c7e6fa32b06d15aa0c2496e177
Baisangur <baisangur.baisarov@narxoz.kz>
2026-03-25 14:27:30 +0500

    book_handler.go file commit

--- bookstore/handlers/book_handler.go
+++ bookstore/handlers/book_handler.go
@@ -3,29 +3,72 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"bookstore/models"
 )
 
-var Authors []models.Author
-var AuthorID = 1
+var Books []models.Book
+var BookID = 1
 
-func GetAuthors(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(Authors)
+func GetBooks(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(Books)
 }
 
-func CreateAuthor(w http.ResponseWriter, r *http.Request) {
-	var author models.Author
-	json.NewDecoder(r.Body).Decode(&author)
+func GetBookByID(w http.ResponseWriter, r *http.Request) {
+	idStr := strings.TrimPrefix(r.URL.Path, "/books/")
+	id, _ := strconv.Atoi(idStr)
 
-	if author.Name == "" {
+	for _, b := range Books {
+		if b.ID == id {
+			json.NewEncoder(w).Encode(b)
+			return
+		}
+	}
+	http.Error(w, "Not found", 404)
+}
+
+func CreateBook(w http.ResponseWriter, r *http.Request) {
+	var book models.Book
+	json.NewDecoder(r.Body).Decode(&book)
+
+	if book.Title == "" || book.Price <= 0 {
 		http.Error(w, "Invalid data", 400)
 		return
 	}
 
-	author.ID = AuthorID
-	AuthorID++
-	Authors = append(Authors, author)
+	book.ID = BookID
+	BookID++
+	Books = append(Books, book)
 
-	json.NewEncoder(w).Encode(author)
+	json.NewEncoder(w).Encode(book)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	idStr := strings.TrimPrefix(r.URL.Path, "/books/")
+	id, _ := strconv.Atoi(idStr)
+
+	for i := range Books {
+		if Books[i].ID == id {
+			json.NewDecoder(r.Body).Decode(&Books[i])
+			Books[i].ID = id
+			json.NewEncoder(w).Encode(Books[i])
+			return
+		}
+	}
+	http.Error(w, "Not found", 404)
+}
+
+func DeleteBook(w http.ResponseWriter, r *http.Request) {
+	idStr := strings.TrimPrefix(r.URL.Path, "/books/")
+	id, _ := strconv.Atoi(idStr)
+
+	for i := range Books {
+		if Books[i].ID == id {
+			Books = append(Books[:i], Books[i+1:]...)
+			return
+		}
+	}
+	http.Error(w, "Not found", 404)
 }
